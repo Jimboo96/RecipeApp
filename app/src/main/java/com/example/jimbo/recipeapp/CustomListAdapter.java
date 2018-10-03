54776cc8678277f8f15f894970f74f6a9f88296c
jimbobjork <t6bjji00@students.oamk.fi>
2018-10-03 19:51:46 +0300

    added some stuff

---
+++ app/src/main/java/com/example/jimbo/recipeapp/CustomListAdapter.java
@@ -0,0 +1,42 @@
+package com.example.jimbo.recipeapp;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomListAdapter extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final String[] itemname;
+    private final String[] linkName;
+    private final Integer[] imgid;
+
+    public CustomListAdapter(Activity context, String[] itemname, String[] linkName, Integer[] imgid) {
+        super(context, R.layout.recipe_view, itemname);
+        // TODO Auto-generated constructor stub
+
+        this.context=context;
+        this.itemname=itemname;
+        this.linkName=linkName;
+        this.imgid=imgid;
+    }
+
+    public View getView(int position,View view,ViewGroup parent) {
+        LayoutInflater inflater=context.getLayoutInflater();
+        View rowView=inflater.inflate(R.layout.recipe_view, null,true);
+
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.Itemname);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+        TextView extratxt = (TextView) rowView.findViewById(R.id.linkName);
+
+        txtTitle.setText(itemname[position]);
+        imageView.setImageResource(imgid[position]);
+        extratxt.setText(linkName[position]);
+        return rowView;
+
+    };
+}
--- app/src/main/java/com/example/jimbo/recipeapp/RecipeView.java
+++ app/src/main/java/com/example/jimbo/recipeapp/RecipeView.java
@@ -1,17 +1,34 @@
 package com.example.jimbo.recipeapp;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class RecipeView extends ListActivity {
+public class RecipeView extends Activity {
     ArrayList<String> titleList = new ArrayList<String>();
     ArrayList<String> imageUrlList = new ArrayList<String>();
     ArrayList<String> recipeUrlList = new ArrayList<String>();
+
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +43,82 @@ public class RecipeView extends ListActivity {
         recipeUrlList = in.getStringArrayListExtra("recipeUrlList");
 
         Object[] objNames = titleList.toArray();
-        String[] strNames = Arrays.copyOf(objNames, objNames.length, String[].class);
+        final String[] strNames = Arrays.copyOf(objNames, objNames.length, String[].class);
 
-        this.setListAdapter(new ArrayAdapter<String>(
-                this, R.layout.recipe_view,
-                R.id.Itemname,strNames));
+        Object[] objNames2 = recipeUrlList.toArray();
+        final String[] strLinks = Arrays.copyOf(objNames2, objNames2.length, String[].class);
+
+        Integer[] imgid={
+                R.drawable.food1,
+                R.drawable.food2,
+                R.drawable.food2,
+                R.drawable.food3,
+                R.drawable.food3,
+                R.drawable.food4,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food5,
+                R.drawable.food6
+        };
+
+
+        //for(int i= 0; i < imageUrlList.size();i++) {
+          //  try {
+                //imgid[i] = drawableFromUrl(imageUrlList.get(i));
+            //} catch (IOException e) {
+              //  e.printStackTrace();
+            //}
+        //}
+
+        //Resources res = getResources();
+        //int resID = res.getIdentifier(imageString , "drawable", getPackageName());
+        //imageView.setImageResource(resID);
+
+        CustomListAdapter adapter=new CustomListAdapter(this, strNames,strLinks, imgid);
+        list=(ListView)findViewById(R.id.listView);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String Slecteditem= strLinks[+position];
+                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    public static Drawable drawableFromUrl(String url) throws IOException {
+        Bitmap x;
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.connect();
+        InputStream input = connection.getInputStream();
+
+        x = BitmapFactory.decodeStream(input);
+        return new BitmapDrawable(x);
     }
 }
